3971d1435f97c214dfa204a47b4f6e8c97482d2c
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-04-26 22:19:17 +0900

    factorize

---
+++ factorize.py
@@ -0,0 +1,24 @@
+from primetest import primetest
+from random import randrange
+from math import gcd
+def factorize(n):
+    def pollard_rho(n):
+        x = randrange(1, n)
+        c = randrange(1, n)
+        y, g = x, 1
+        while g == 1:
+            x = (x * x + c) % n
+            y = (y * y + c) % n
+            y = (y * y + c) % n
+            g = gcd(abs(x - y), n)
+        if g == n:
+            return pollard_rho(n)
+        return g
+    if n == 1:
+        return []
+    if not n & 1:
+        return [2] + factorize(n // 2)
+    if primetest(n):
+        return [n]
+    f = pollard_rho(n)
+    return factorize(f) + factorize(n // f)
