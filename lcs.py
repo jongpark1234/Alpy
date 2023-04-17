f56eefdb61d3481fd70110179f60acf453a29d55
JongPark <jongpark1234@naver.com>
2023-04-17 21:01:27 +0900

    create getDPTable prototype

--- lcs.py
+++ lcs.py
@@ -20,6 +20,17 @@ class LCS:
             if si == 0 or ti == 0:
                 break
         return '' if dp[sLength][tLength] == 0 else ''.join(map(str, result))
+    def getDPTable(s: str, t: str) -> str:
+        a = input()
+        b = input()
+        dp = [[0 for _ in range(len(a) + 1)] for _ in range(len(b) + 1)]
+        for i in range(1, len(b) + 1):
+            for j in range(1, len(a) + 1):
+                dp[i][j] = dp[i - 1][j - 1] + 1 if a[j - 1] == b[i - 1] else max(dp[i][j - 1], dp[i - 1][j])
+        print('  i ' + ' '.join(map(str, range(len(a) + 1))))
+        print('/ - ' + ' '.join(a))
+        for i in range(len(b) + 1):
+            print('-' if i == 0 else b[i - 1], *dp[i])
     def processWithHirschBurg(s: str, t: str) -> str:
         def hirschburg(s: str, sLength1: int, sLength2: int, t: str, tLength1: int, tLength2: int) -> str:
             ret, maxValue = '', -float('inf')
