0246f5aeaecc46b986dcaab3b56a096bc8c39a4b
JongPark <jongpark1234@naver.com>
2024-04-18 16:58:23 +0900

    Removed Mapping Function at Two-Sat

--- twosat.py
+++ twosat.py
@@ -13,34 +13,6 @@ class TwoSat:
         """
         self._graph = [set() for _ in range(var_count * 2)]
         self._var_ord = self._neg_var_ord = None
-    
-    def _mapping(self, x: int) -> int:
-        """
-        해당 구문은 주어진 변수 x에 대해 참임을 나타내는 정점을 그래프에 추가하기 전에 변수의 인덱스를 적절히 매핑하는 과정을 수행합니다.
-        
-        구문을 자세히 설명하면 다음과 같습니다:
-
-        x가 음수인 경우:
-            음수 변수에 대해서는 부정을 나타내는 정점을 생성해야 합니다. 따라서 음수 변수에 대해 해당 변수를 양수로 바꾼 뒤, 부정 연산을 수행합니다.
-            
-            x가 음수인 경우, x + x를 계산하면 양수 값이 됩니다.
-            
-            부정 연산을 위해 -1 - x - x를 수행합니다. 이것은 x의 부정 값을 의미합니다.
-
-        x가 양수인 경우:
-            양수 변수는 해당 변수를 그대로 사용합니다.
-
-            x가 양수인 경우, x + x를 계산하면 2를 곱한 값이 됩니다. 따라서 이 경우에는 변수를 그대로 사용합니다.
-        
-        Args:
-            x (int): 특정 변수의 인덱스입니다.
-        
-        Returns:
-            int: 매핑된 그래프의 인덱스를 반환합니다.
-        """
-
-        return x + x if x >= 0 else -1 - x - x
-
 
     def x_is_true(self, x: int) -> None:
         """
@@ -49,9 +21,6 @@ class TwoSat:
         Args:
             x (int): 참임을 나타내는 변수의 인덱스입니다.
         """
-
-        # 변수의 인덱스를 그래프 상의 정점으로 매핑
-        x = self._mapping(x)
 
         # x 변수가 참일 때, 그래프에 해당 변수의 부정을 나타내는 정점 추가
         self._graph[x ^ 1].add(x)
@@ -64,10 +33,6 @@ class TwoSat:
             x (int): 첫 번째 변수의 인덱스입니다.
             y (int): 두 번째 변수의 인덱스입니다.
         """
-
-        # x와 y를 매핑하여 그래프 상의 정점으로 변환
-        x = self._mapping(x)
-        y = self._mapping(y)
         
         # x 또는 y 중 적어도 하나가 참일 때, 서로의 부정을 나타내는 정점 추가
         self._graph[x ^ 1].add(y)
@@ -82,10 +47,6 @@ class TwoSat:
             y (int): 두 번째 변수의 인덱스입니다.
         """
 
-        # x와 y를 매핑하여 그래프 상의 정점으로 변환
-        x = self._mapping(x)
-        y = self._mapping(y)
-
          # x와 y가 동시에 참이거나 동시에 거짓일 때, 해당 관계를 나타내는 간선 추가
         self._graph[x].add(y)
         self._graph[x ^ 1].add(y ^ 1)
@@ -99,9 +60,6 @@ class TwoSat:
         Args:
             x (list[int]): 변수들의 인덱스로 이루어진 리스트입니다.
         """
-
-        # 변수들을 그래프 상의 정점으로 매핑
-        x = [self._mapping(i) for i in x]
 
         # 변수의 개수가 4 이하인 경우 모든 변수 쌍에 대해 서로 부정을 나타내는 간선 추가
         if len(x) <= 4:
@@ -149,9 +107,9 @@ class TwoSat:
             list[bool]: 충족 가능한 경우 변수에 대한 참 값의 할당을 나타내는 리스트입니다.
         """
         
-        # 문제가 충족 불가능하다면 ValueError를 Raise
+        # 문제가 충족 불가능하다면 None을 반환
         if not self.is_satisfiable():
-            raise ValueError('No truth assignment exists.')
+            return None
         
         # 충족 가능한 경우, 변수에 대한 참 값의 할당을 생성하여 반환
         # 각 변수와 그 부정 변수의 순서를 비교하여 참 값의 할당을 결정
